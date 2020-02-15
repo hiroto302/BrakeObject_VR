d570953563636a63b3b23540a957f2ad56012d2a
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-15 17:14:47 +0900

    魔法生成の位置
    handの子オブジェクトとして生成

--- Assets/Scripts/MagicController.cs
+++ Assets/Scripts/MagicController.cs
@@ -6,25 +6,30 @@ public class MagicController : MonoBehaviour
 {
     public GameObject magicPrefab;
     GameObject breakObject;
+
     void Start()
     {
+        this.breakObject.GetComponent<BoxCollider>().enabled = true;
         this.breakObject = GameObject.Find("BreakableCube");
     }
 
     void Update()
     {
-        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
             OVRInput.SetControllerVibration(0.3f, 0.3f, OVRInput.Controller.LTouch);
-            Instantiate(magicPrefab, gameObject.transform.position, Quaternion.identity);
+            Instantiate(magicPrefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
             this.breakObject.GetComponent<BoxCollider>().enabled = true;
             // this.breakObject.GetComponent<BoxCollider>().isTrigger = true;
         }
-        else if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger))
+        else if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
         {
             OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
             this.breakObject.GetComponent<BoxCollider>().enabled = false;
             // this.breakObject.GetComponent<BoxCollider>().isTrigger = false;
         }
+
+
     }
 }
